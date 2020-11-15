e6c8e3cd7df32138219466dc0146cae4dc9826b9
morteza-kh <morteza.kh.r@gmail.com>
2020-11-16 00:44:28 +0330

    # cli args

--- create_header_file.cpp
+++ create_header_file.cpp
@@ -4,9 +4,11 @@
 #include <fstream>
 #include <iostream>
 #include <regex>
+#include <string>
+#include <vector>
 
 #if BOOST_OS_WINDOWS
-#	include <Windows.h>
+#include <Windows.h>
 #endif
 
 #include <boost/algorithm/string.hpp>
@@ -14,19 +16,19 @@
 #include <boost/uuid/random_generator.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
-
 auto main(int argc, char const **argv) -> int {
-	auto make_header = [](auto file_name) {
-		std::ofstream ofs { file_name };
-		auto uuid  = boost::algorithm::to_upper_copy(boost::uuids::to_string(boost::uuids::random_generator()()));
-		auto guard = "I" + std::regex_replace(uuid, std::regex { "-" }, "_");
+	auto header_names = std::vector<std::string> { &argv[1], &argv[argc] };
+	auto make_header  = [](auto const &file_name) {
+		 std::ofstream ofs { file_name };
+		 auto uuid	= boost::algorithm::to_upper_copy(boost::uuids::to_string(boost::uuids::random_generator()()));
+		 auto guard = "I" + std::regex_replace(uuid, std::regex { "-" }, "_");
 
-		ofs << "#ifndef " + guard + "\n#define " + guard + "\n\n#endif /* !" + guard + " */";
-		ofs.close();
+		 ofs << "#ifndef " + guard + "\n#define " + guard + "\n\n#endif /* !" + guard + " */";
+		 ofs.close();
 	};
 
-	for (int i = 1; i < argc; i++) {
-		make_header(argv[i]);
+	for (auto const &header_name : header_names) {
+		make_header(header_name);
 	}
 
 	return 0;
